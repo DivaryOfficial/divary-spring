4ad8b3be72c3e3555a626383b614890e338b56d9
qkrcodus <qkrcodus0722@gmail.com>
2025-07-07 17:04:08 +0900

    chore: 불필요한 schema.sql 파일 제거

--- src/main/resources/schema.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE IF NOT EXISTS test_connection_log (
-                                                   id BIGINT AUTO_INCREMENT PRIMARY KEY,
-                                                   message VARCHAR(255)
-    );
