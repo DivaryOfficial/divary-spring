d454d786e5c67aa8e52513fadadbc16e838a6568
Azin <setiguy@gachon.ac.kr>
2025-07-08 15:33:29 +0900

    중간 백업
    로컬에서는 되는데 ec2 주소로는 안됨

---
+++ src/main/java/com/divary/global/oauth/service/social/SocialOauth.java
@@ -0,0 +1,18 @@
+package com.divary.global.oauth.service.social;
+
+import com.divary.common.enums.SocialType;
+
+public interface SocialOauth {
+    String getOauthRedirectURL();
+    String requestAccessToken(String code);
+
+    default SocialType type() {
+        if (this instanceof GoogleOauth) { //다른 소셜 로그인 추가 가능
+            return SocialType.GOOGLE;
+        } else {
+            System.out.println("NuLL soical Type");
+            return null;
+        }
+    }
+
+}
