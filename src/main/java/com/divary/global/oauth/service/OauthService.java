12bc0f9bb995742c609a0e8e878bd5f08295a959
Azin <setiguy@gachon.ac.kr>
2025-07-08 15:33:29 +0900

    중간 백업
    로컬에서는 되는데 ec2 주소로는 안됨

---
+++ src/main/java/com/divary/global/oauth/service/OauthService.java
@@ -0,0 +1,39 @@
+package com.divary.global.oauth.service;
+
+import com.divary.common.enums.SocialType;
+import com.divary.global.oauth.service.social.SocialOauth;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class OauthService {
+    private final List<SocialOauth> socialOauthList;
+    private final HttpServletResponse response;
+
+    public void request(SocialType socialLoginType) {
+        SocialOauth socialOauth = this.findSocialOauthByType(socialLoginType);
+        String redirectURL = socialOauth.getOauthRedirectURL();
+        try {
+            response.sendRedirect(redirectURL);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String requestAccessToken(SocialType socialLoginType, String code) {
+        SocialOauth socialOauth = this.findSocialOauthByType(socialLoginType);
+        return socialOauth.requestAccessToken(code);
+    }
+
+    private SocialOauth findSocialOauthByType(SocialType socialLoginType) {
+        return socialOauthList.stream()
+                .filter(x -> x.type() == socialLoginType)
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("알 수 없는 SocialLoginType 입니다."));
+    }
+}
