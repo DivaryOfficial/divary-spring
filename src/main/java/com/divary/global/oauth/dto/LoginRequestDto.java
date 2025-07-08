64c4e2e0a93431400c1c1efdaec91e8973d7a06b
Azin <setiguy@gachon.ac.kr>
2025-07-09 00:55:16 +0900

    수정중...

---
+++ src/main/java/com/divary/global/oauth/dto/LoginRequestDto.java
@@ -0,0 +1,5 @@
+package com.divary.global.oauth.dto;
+
+public class LoginRequestDto {
+    private String accessToken;
+}
--- src/main/java/com/divary/global/oauth/service/social/GoogleOauth.java
+++ src/main/java/com/divary/global/oauth/service/social/GoogleOauth.java
@@ -14,55 +14,8 @@ import java.util.stream.Collectors;
 @Component
 @RequiredArgsConstructor
 public class GoogleOauth implements SocialOauth {
-    @Value("${sns.google.url}")
-    private String GOOGLE_SNS_BASE_URL;
-    @Value("${sns.google.client.id}")
-    private String GOOGLE_SNS_CLIENT_ID;
-    @Value("${sns.google.callback.url}")
-    private String GOOGLE_SNS_CALLBACK_URL;
-    @Value("${sns.google.client.secret}")
-    private String GOOGLE_SNS_CLIENT_SECRET;
-    @Value("${sns.google.token.url}")
-    private String GOOGLE_SNS_TOKEN_BASE_URL;
 
-    @Override
-    public String getOauthRedirectURL() {
-        Map<String, Object> params = new HashMap<>();
-        params.put("scope", "openid email profile");
-        params.put("response_type", "code");
-        params.put("client_id", GOOGLE_SNS_CLIENT_ID);
-        params.put("redirect_uri", GOOGLE_SNS_CALLBACK_URL);
 
-        String parameterString = params.entrySet().stream()
-                .map(x -> x.getKey() + "=" + x.getValue())
-                .collect(Collectors.joining("&"));
-
-        return GOOGLE_SNS_BASE_URL + "?" + parameterString;
-    }
-
-    @Override
-    public String requestAccessToken(String code) {
-        RestTemplate restTemplate = new RestTemplate();
-
-        Map<String, Object> params = new HashMap<>();
-        params.put("code", code);
-        params.put("client_id", GOOGLE_SNS_CLIENT_ID);
-        params.put("client_secret", GOOGLE_SNS_CLIENT_SECRET);
-        params.put("redirect_uri", GOOGLE_SNS_CALLBACK_URL);
-        params.put("grant_type", "authorization_code");
-
-        ResponseEntity<Map> responseEntity = restTemplate.postForEntity(GOOGLE_SNS_TOKEN_BASE_URL, params, Map.class);
-
-        if (responseEntity.getStatusCode() == HttpStatus.OK) {
-            Map body = responseEntity.getBody();
-            String accessToken = (String) body.get("access_token");
-            String idToken = (String) body.get("id_token");
-
-            // 사용자 정보 요청
-            return requestUserInfo(accessToken);
-        }
-        return "구글 로그인 요청 처리 실패";
-    }
     private String requestUserInfo(String accessToken) {
         RestTemplate restTemplate = new RestTemplate();
         String userInfoUrl = "https://www.googleapis.com/oauth2/v2/userinfo";
@@ -88,6 +41,11 @@ public class GoogleOauth implements SocialOauth {
         }
 
         return "사용자 정보 조회 실패";
+    }
+    @Override
+    public String verifyAndLogin(String accessToken) {
+        // accessToken으로 사용자 정보 요청
+        return requestUserInfo(accessToken);
     }
 
 }
