42c802d1fe9f8791e54a3676126ad8f6f91146f3
Azin <setiguy@gachon.ac.kr>
2025-07-09 01:10:57 +0900

    service 작성
    findMemberByEmail, findById 작성

--- src/main/java/com/divary/domain/Member/repository/MemberRepository.java
+++ src/main/java/com/divary/domain/Member/repository/MemberRepository.java
@@ -3,12 +3,8 @@ package com.divary.domain.Member.repository;
 import com.divary.domain.Member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-<<<<<<< HEAD
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByEmail(String email);
-=======
-public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom {
->>>>>>> 25fb5bd (유저 엔티티 작성 및 repository 생성)
 }
