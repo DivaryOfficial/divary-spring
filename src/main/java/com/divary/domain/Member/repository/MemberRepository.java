553a72b8422b813b02b0907edfac284cc5e7088a
Azin <setiguy@gachon.ac.kr>
2025-07-09 01:10:57 +0900

    service 작성
    findMemberByEmail, findById 작성

--- src/main/java/com/divary/domain/Member/repository/MemberRepository.java
+++ src/main/java/com/divary/domain/Member/repository/MemberRepository.java
@@ -3,5 +3,8 @@ package com.divary.domain.Member.repository;
 import com.divary.domain.Member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom {
+    Optional<Member> findByEmail(String email);
 }
---
+++ src/main/java/com/divary/domain/Member/service/MemberServiceImpl.java
@@ -0,0 +1,21 @@
+package com.divary.domain.Member.service;
+
+import com.divary.domain.Member.entity.Member;
+import com.divary.domain.Member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemberServiceImpl implements MemberService {
+    private final MemberRepository memberRepository;
+    @Override
+    public Member findMemberByEmail(String email) {
+        return memberRepository.findByEmail(email).orElseThrow(()-> new IllegalArgumentException("Email not found"));
+    }
+
+    @Override
+    public Member findById(Long id) {
+        return  memberRepository.findById(id).orElseThrow(()-> new IllegalArgumentException("Member id not found"));
+    }
+}
