6912004f00e08e31de7954fe96ede3b7ce5e841f
Azin <setiguy@gachon.ac.kr>
2025-07-09 00:55:16 +0900

    수정중...

--- src/main/java/com/divary/global/oauth/service/social/SocialOauth.java
+++ src/main/java/com/divary/global/oauth/service/social/SocialOauth.java
@@ -3,8 +3,7 @@ package com.divary.global.oauth.service.social;
 import com.divary.common.enums.SocialType;
 
 public interface SocialOauth {
-    String getOauthRedirectURL();
-    String requestAccessToken(String code);
+    String verifyAndLogin(String code);
 
     default SocialType type() {
         if (this instanceof GoogleOauth) { //다른 소셜 로그인 추가 가능
