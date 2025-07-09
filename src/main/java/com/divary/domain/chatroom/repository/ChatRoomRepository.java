5047439fd36e567dc7a9d0bbee5c522397485a71
qkrcodus <qkrcodus0722@gmail.com>
2025-07-09 21:16:31 +0900

    docs: ChatRoom 관련 런타임 에러 주석 처리 한 것 해지

--- src/main/java/com/divary/domain/chatroom/repository/ChatRoomRepository.java
+++ src/main/java/com/divary/domain/chatroom/repository/ChatRoomRepository.java
@@ -1,10 +1,10 @@
-
 package com.divary.domain.chatroom.repository;
 
 import com.divary.domain.chatroom.entity.ChatRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+
 
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
 
