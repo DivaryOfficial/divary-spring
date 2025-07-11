0c834e239b7de3bef753f9c37a6e96b74761f4c3
Azin <setiguy@gachon.ac.kr>
2025-07-11 18:35:29 +0900

    feat/#17: jwt 작성
    accsess 4시간
    refresh 7일
    로 일단 해놨습니다.

--- src/main/java/com/divary/global/oauth/service/social/GoogleOauth.java
+++ src/main/java/com/divary/global/oauth/service/social/GoogleOauth.java
@@ -4,23 +4,28 @@ import com.divary.common.enums.SocialType;
 import com.divary.domain.Member.entity.Member;
 import com.divary.domain.Member.enums.Role;
 import com.divary.domain.Member.service.MemberService;
+import com.divary.global.config.security.jwt.JwtTokenProvider;
 import com.divary.global.oauth.dto.LoginResponseDTO;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.HashMap;
+import java.util.Collections;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 @Component
 @RequiredArgsConstructor
 public class GoogleOauth implements SocialOauth {
 
     private final MemberService memberService;
+    private final JwtTokenProvider jwtTokenProvider;
+
 
     private Map<String, Object> requestUserInfo(String accessToken) {
         RestTemplate restTemplate = new RestTemplate();
@@ -50,9 +55,9 @@ public class GoogleOauth implements SocialOauth {
     }
 
     @Override
-    public LoginResponseDTO verifyAndLogin(String accessToken) {
+    public LoginResponseDTO verifyAndLogin(String googleAccessToken) {
         // accessToken으로 사용자 정보 요청
-        Map<String, Object> userInfo = requestUserInfo(accessToken);
+        Map<String, Object> userInfo = requestUserInfo(googleAccessToken);
         String email = (String) userInfo.get("email");
         String name = (String)userInfo.get("name");
 
@@ -70,14 +75,16 @@ public class GoogleOauth implements SocialOauth {
 
         //todo JWT 토큰 관리 추가
 
+        Authentication authentication = new UsernamePasswordAuthenticationToken(
+                email, null,
+                Collections.singleton(new SimpleGrantedAuthority("ROLE_" + Role.User.name()))  // 권한을 설정 일시적으로 일반 유저 권한만
+        );
+
+        // JWT 토큰 발급
+        String accessToken = jwtTokenProvider.generateToken(authentication);
+
         // 3. 응답 생성
-        return LoginResponseDTO.builder()
-                .email(member.getEmail())
-                .id(member.getId())
-                .name(name)
-                .socialType(member.getSocialType())
-                .role(member.getRole())
-                .build();
+        return LoginResponseDTO.builder().token(accessToken).build();
 
     }
 }
