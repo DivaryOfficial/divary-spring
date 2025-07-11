109fe8cc4dac74bbd765f141b648fcbdeaa6c4cd
Baguette-bbang <dudals9701@naver.com>
2025-07-11 18:00:04 +0900

    refactor : 이미지 타입에 경로 설정 추가

--- src/main/java/com/divary/domain/image/entity/ImageType.java
+++ src/main/java/com/divary/domain/image/entity/ImageType.java
@@ -1,9 +1,23 @@
 package com.divary.domain.image.entity;
 
+import lombok.Getter;
+
+@Getter
 public enum ImageType {
-    DIVING_LOG, // 다이빙 로그 이미지
-    CHAT, // 채팅 이미지
-    LISENCE, // 라이센스 이미지
-    DOGAM, // 도감 이미지
-    DOGAM_PROFILE, // 도감 프로필 이미지
+    // 유저 이미지
+    USER_DIVING_LOG("users/{userId}/logbook/{datePath}/{fileName}", true),
+    USER_CHAT("users/{userId}/chat/{fileName}", true),
+    USER_LICENSE("users/{userId}/license/{fileName}", true),
+    
+    // 시스템 이미지 (모든 유저에게 공통인 것들을 처리하면 됩니다.)
+    SYSTEM_DOGAM("system/dogam/{type}/{fileName}", false),
+    SYSTEM_DOGAM_PROFILE("system/dogam/profile/{fileName}", false);
+
+    private final String pathPattern;
+    private final boolean requiresUserId;
+
+    ImageType(String pathPattern, boolean requiresUserId) {
+        this.pathPattern = pathPattern;
+        this.requiresUserId = requiresUserId;
+    }
 }
