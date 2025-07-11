e984031c9aa826d33d2faf0af2de10dc30902aae
Azin <setiguy@gachon.ac.kr>
2025-07-11 10:43:35 +0900

    feat/#17: todo 추가

--- src/main/java/com/divary/global/oauth/controller/OauthController.java
+++ src/main/java/com/divary/global/oauth/controller/OauthController.java
@@ -6,6 +6,7 @@ import com.divary.common.response.ApiResponse;
 import com.divary.global.oauth.dto.LoginRequestDto;
 import com.divary.global.oauth.dto.LoginResponseDTO;
 import com.divary.global.oauth.service.OauthService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -19,13 +20,9 @@ import org.springframework.web.bind.annotation.*;
 public class OauthController {
     private final OauthService oauthService;
 
-//    @GetMapping(value = "/{socialLoginType}")
-//    public void socialLoginType(@PathVariable(name = "socialLoginType") SocialType socialLoginType) {
-//        log.info(">> 사용자로부터 SNS 로그인 요청을 받음 :: {} Social Login", socialLoginType);
-//        oauthService.request(socialLoginType);
-//    }
 
     @PostMapping(value = "/{socialLoginType}/login")
+    @Operation(summary = "로그인", description = "accessCode를 보내주세요")
     public ApiResponse<LoginResponseDTO> login(@PathVariable(name = "socialLoginType") SocialType socialLoginType,
                                      @RequestBody LoginRequestDto loginRequestDto) {
         String accessToken = loginRequestDto.getAccessToken();
