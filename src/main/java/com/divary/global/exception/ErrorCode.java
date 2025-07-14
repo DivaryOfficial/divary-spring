c8e498b156c62bd18ce4ca0b94da03cb56ee0098
Azin <setiguy@gachon.ac.kr>
2025-07-15 05:57:31 +0900

    feat/#17: enum 검증 생성, error코드 사용하게 변경

--- src/main/java/com/divary/global/exception/ErrorCode.java
+++ src/main/java/com/divary/global/exception/ErrorCode.java
@@ -16,12 +16,15 @@ public enum ErrorCode {
     // 실제 사용되는 검증 에러들
     VALIDATION_ERROR(HttpStatus.BAD_REQUEST, "VALIDATION_001", "입력값 검증에 실패했습니다."),
     REQUIRED_FIELD_MISSING(HttpStatus.BAD_REQUEST, "VALIDATION_002", "필수 필드가 누락되었습니다."),
-    INVALID_TOKEN(HttpStatus.BAD_REQUEST, "VALIDATION_003", "토큰이 잘못되었습니다.");
+    INVALID_TOKEN(HttpStatus.BAD_REQUEST, "VALIDATION_003", "토큰이 잘못되었습니다."),
 
     // TODO: 비즈니스 로직 개발하면서 필요한 에러코드들 추가
     CARD_NOT_FOUND(HttpStatus.NOT_FOUND, "ENCYCLOPEDIA_001", "해당 카드에 대한 정보를 찾을 수 없습니다."),
     TYPE_NOT_FOUND(HttpStatus.NOT_FOUND, "ENCYCLOPEDIA_002", "존재하지 않는 종류입니다."),
-    
+    EMAIL_NOT_FOUND(HttpStatus.NOT_FOUND, "MEMBER_001", "이메일을 찾을 수 없습니다."),
+    MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "MEMBER_002", "유저를 찾을 수 없습니다."),
+    NOTIFICAITION_NOT_FOUND(HttpStatus.NOT_FOUND, "MEMBER_003", "해당 ID를 가진 사용자의 알림이 존재하지 않습니다"),
+
     // 채팅방 관련 에러코드
     CHAT_ROOM_NOT_FOUND(HttpStatus.NOT_FOUND, "CHAT_ROOM_001", "채팅방을 찾을 수 없습니다.");
 
--- src/main/java/com/divary/global/oauth/service/social/SocialOauth.java
+++ src/main/java/com/divary/global/oauth/service/social/SocialOauth.java
@@ -5,6 +5,7 @@ import com.divary.global.oauth.dto.LoginResponseDTO;
 
 public interface SocialOauth {
     LoginResponseDTO verifyAndLogin(String code);
+    SocialType type = null;
 
     default SocialType type() {
         if (this instanceof GoogleOauth) { //다른 소셜 로그인 추가 가능
