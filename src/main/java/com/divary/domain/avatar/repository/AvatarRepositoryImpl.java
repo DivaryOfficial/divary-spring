ec256822c0cb514a50ef074607f3f98b89bd18b9
Azin <setiguy@gachon.ac.kr>
2025-07-17 00:03:12 +0900

    feat/#58: get 작성 완료

--- src/main/java/com/divary/domain/avatar/repository/AvatarRepositoryImpl.java
+++ src/main/java/com/divary/domain/avatar/repository/AvatarRepositoryImpl.java
@@ -4,9 +4,13 @@ import com.divary.domain.Member.entity.Member;
 import com.divary.domain.avatar.entity.Avatar;
 import com.divary.global.exception.BusinessException;
 import com.divary.global.exception.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
 
+@Repository
+@RequiredArgsConstructor
 public class AvatarRepositoryImpl implements AvatarRepositoryCustom{
-    private AvatarRepository avatarRepository;
+    private final AvatarRepository avatarRepository;
 
     @Override
     public Avatar saveByMember(Member member, Avatar updatedAvatar) {
