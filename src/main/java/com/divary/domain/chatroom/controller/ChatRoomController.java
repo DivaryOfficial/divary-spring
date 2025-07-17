a7a89e141331dd4324a2f221f61d3c788b7464c5
Baguette-bbang <dudals9701@naver.com>
2025-07-17 23:14:38 +0900

    feat : 전역적으로 /api/v1 프리픽스 추가

--- src/main/java/com/divary/domain/chatroom/controller/ChatRoomController.java
+++ src/main/java/com/divary/domain/chatroom/controller/ChatRoomController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/chatrooms")
+@RequestMapping("chatrooms")
 @RequiredArgsConstructor
 @Tag(name = "ChatRoom", description = "채팅방 API")
 public class ChatRoomController {
