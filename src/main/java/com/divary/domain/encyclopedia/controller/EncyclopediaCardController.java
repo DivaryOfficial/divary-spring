1b799cf17d13b2f700b7b787324406993d80b876
qkrcodus <qkrcodus0722@gmail.com>
2025-07-18 11:37:47 +0900

    refactor: 카드 API 엔드포인트를 /api/v1/cards → /cards 로 변경

--- src/main/java/com/divary/domain/encyclopedia/controller/EncyclopediaCardController.java
+++ src/main/java/com/divary/domain/encyclopedia/controller/EncyclopediaCardController.java
@@ -15,7 +15,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/cards")
+@RequestMapping("/cards")
 @RequiredArgsConstructor
 public class EncyclopediaCardController {
 
