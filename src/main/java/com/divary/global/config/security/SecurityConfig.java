c0a018be276b1178c8060b9c1c724c3c40fb5160
Azin <setiguy@gachon.ac.kr>
2025-07-20 22:28:38 +0900

    feat/#17: jwt 인증 추가
    혹시 테스트 해보실 분은 notification 경로로 테스트해보시면 됩니다! 임시로 해놨어용

--- src/main/java/com/divary/global/config/security/SecurityConfig.java
+++ src/main/java/com/divary/global/config/security/SecurityConfig.java
@@ -1,32 +1,38 @@
 package com.divary.global.config.security;
 
+import com.divary.global.config.security.jwt.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
     @Bean
-    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(authorizeHttpRequests ->
-                authorizeHttpRequests
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.ignoringRequestMatchers("/h2-console/**").disable())
+
+                .headers(headers -> headers.frameOptions().sameOrigin()) // ← H2 iframe 허용
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/v1/notification").authenticated()
                         .anyRequest().permitAll()
-        );
-
-
-        //임시로 꺼둠
-        http.csrf((auth) -> auth.disable());
-
-        return http.build();
+                )
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
     }
-    @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder(){
 
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
 }
