ac17f05b4981452c31aefc8e19fb1c2ea5b462f0
Azin <setiguy@gachon.ac.kr>
2025-07-20 22:28:38 +0900

    feat/#17: jwt 인증 추가
    혹시 테스트 해보실 분은 notification 경로로 테스트해보시면 됩니다! 임시로 해놨어용

--- src/main/java/com/divary/domain/notification/controller/NotificationController.java
+++ src/main/java/com/divary/domain/notification/controller/NotificationController.java
@@ -3,6 +3,7 @@ package com.divary.domain.notification.controller;
 import com.divary.common.response.ApiResponse;
 import com.divary.domain.notification.dto.NotificationResponseDTO;
 import com.divary.domain.notification.service.NotificationService;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/notification")
 @RequiredArgsConstructor
+@SecurityRequirement(name = "JWT") // jwt 필요시 작성
 public class NotificationController {
     private final NotificationService notificationService;
 
