5b40e9377ba707cdf9ec789d3ccf60a77249504f
Baguette-bbang <dudals9701@naver.com>
2025-07-23 00:13:29 +0900

    feat/#55: 커스텀 인증 구현체 추가 및 JWT 인증 로직 개선

---
+++ src/main/java/com/divary/global/config/security/CustomUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.divary.global.config.security;
+
+import com.divary.domain.Member.entity.Member;
+import com.divary.domain.Member.repository.MemberRepository;
+import com.divary.global.exception.BusinessException;
+import com.divary.global.exception.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    
+    private final MemberRepository memberRepository;
+    
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Member member = memberRepository.findByEmail(email)
+                .orElseThrow(() -> new BusinessException(ErrorCode.MEMBER_NOT_FOUND));
+        
+        return new CustomUserPrincipal(member);
+    }
+}
