172db369bdb6ce1cb031ec70bbee4dc53b040f09
qkrcodus <qkrcodus0722@gmail.com>
2025-07-24 12:01:10 +0900

    refactor: getCards 에서 반복 호출중이던 imageService 제거 -> SYSTEM_DOGAM_PROFILE 일괄 조회

--- src/main/java/com/divary/domain/encyclopedia/dto/EncyclopediaCardSummaryResponse.java
+++ src/main/java/com/divary/domain/encyclopedia/dto/EncyclopediaCardSummaryResponse.java
@@ -18,7 +18,7 @@ public class EncyclopediaCardSummaryResponse {
     @Schema(description = "생물 종류", example = "어류")
     private String type;
 
-    @Schema(description = "썸네일 이미지 URL", example = "\"https://s3.example.com/card1-thumbnail.jpg\"")
-    private String thumbnailUrl;
+    @Schema(description = "도감 이모지 프로필 URL", example = "\"https://s3.example.com/card1-thumbnail.jpg\"")
+    private String dogamProfileUrl;
 
 }
