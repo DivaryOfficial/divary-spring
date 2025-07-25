cfd3d27a0d5ada6c1692c9342324c297c8a9d780
qkrcodus <qkrcodus0722@gmail.com>
2025-07-25 13:12:51 +0900

    refactor: 안쓰이는 enums 패키지 제거

--- src/main/java/com/divary/domain/diary/enums/FontType.java
+++
@@ -1,5 +0,0 @@
-package com.divary.domain.diary.enums;
-
-public enum FontType {
-    BASIC, YEONMU, ONGUL, CAFE24, NANUM
-}
--- src/main/java/com/divary/domain/diary/enums/TextAlign.java
+++
@@ -1,5 +0,0 @@
-package com.divary.domain.diary.enums;
-
-public enum TextAlign {
-    LEFT, CENTER, RIGHT
-}
