791349b62d46d377f5cc09ba2a18606bd85077f6
Azin <setiguy@gachon.ac.kr>
2025-07-31 11:25:14 +0900

    feat/#75: 멤버 도메인으로 이동, 이미지는 /api/v1/images/upload/type/{imageType}로 관리

--- src/main/java/com/divary/domain/member/service/MemberService.java
+++ src/main/java/com/divary/domain/member/service/MemberService.java
@@ -1,9 +1,11 @@
 package com.divary.domain.member.service;
 
 import com.divary.domain.member.entity.Member;
+import com.divary.domain.member.dto.requestDTO.MyPageLevelRequestDTO;
 
 public interface MemberService {
     Member findMemberByEmail(String email);
     Member findById(Long id);
     Member saveMember(Member member);
+    void updateLevel(Long userId, MyPageLevelRequestDTO requestDTO);
 }
