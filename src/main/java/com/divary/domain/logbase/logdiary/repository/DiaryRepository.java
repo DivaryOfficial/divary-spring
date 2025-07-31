a53c8ac727105715c5357bcec6bc80429759ebc4
qkrcodus <qkrcodus0722@gmail.com>
2025-07-31 18:15:28 +0900

    refactor: @EntityGraph 제거

--- src/main/java/com/divary/domain/logbase/logdiary/repository/DiaryRepository.java
+++ src/main/java/com/divary/domain/logbase/logdiary/repository/DiaryRepository.java
@@ -2,16 +2,11 @@ package com.divary.domain.logbase.logdiary.repository;
 
 import com.divary.domain.logbase.logdiary.entity.Diary;
 import java.util.Optional;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface DiaryRepository extends JpaRepository<Diary, Long> {
-    @EntityGraph(attributePaths = {
-            "logBaseInfo",
-            "logBaseInfo.member"
-    })
     boolean existsByLogBaseInfoId(Long logBaseInfoId);
 
     @Query("SELECT d FROM Diary d WHERE d.logBaseInfo.id = :logBaseInfoId")
