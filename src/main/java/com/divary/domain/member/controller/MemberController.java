38691eb04f5d96df8ab70392d468ea1fe54e5efb
Azin <setiguy@gachon.ac.kr>
2025-08-01 17:03:15 +0900

    feat/#75: controller 수정

--- src/main/java/com/divary/domain/member/controller/MemberController.java
+++ src/main/java/com/divary/domain/member/controller/MemberController.java
@@ -1,18 +1,19 @@
 package com.divary.domain.member.controller;
 
 import com.divary.common.response.ApiResponse;
-import com.divary.domain.member.dto.requestDTO.MyPageImageRequestDTO;
 import com.divary.domain.member.dto.requestDTO.MyPageLevelRequestDTO;
 import com.divary.domain.member.dto.response.MyPageImageResponseDTO;
 import com.divary.global.config.SwaggerConfig;
 import com.divary.global.config.security.CustomUserPrincipal;
 import com.divary.global.exception.ErrorCode;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import com.divary.domain.member.service.MemberService;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController("/member")
 @RequiredArgsConstructor
@@ -30,10 +31,9 @@ public class MemberController {
     @PostMapping(path = "/license", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     @SwaggerConfig.ApiSuccessResponse(dataType = MyPageImageResponseDTO.class)
     @SwaggerConfig.ApiErrorExamples(value = {ErrorCode.INVALID_INPUT_VALUE, ErrorCode.AUTHENTICATION_REQUIRED})
-    public ApiResponse licenseUpload(@AuthenticationPrincipal CustomUserPrincipal userPrincipal, @Valid @ModelAttribute MyPageImageRequestDTO requestDTO) {
+    public ApiResponse licenseUpload(@AuthenticationPrincipal CustomUserPrincipal userPrincipal, @RequestPart("image") @NotNull MultipartFile image) {
 
-
-       MyPageImageResponseDTO response = memberService.uploadLicense(requestDTO.getImage(), userPrincipal.getId());
+        MyPageImageResponseDTO response = memberService.uploadLicense(image, userPrincipal.getId());
 
         return ApiResponse.success(response);
     }
--- src/main/java/com/divary/domain/member/dto/response/MyPageImageResponseDTO.java
+++ src/main/java/com/divary/domain/member/dto/response/MyPageImageResponseDTO.java
@@ -1,9 +1,12 @@
 package com.divary.domain.member.dto.response;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.Setter;
 
 @AllArgsConstructor
+@Setter
+@Getter
 public class MyPageImageResponseDTO {
     String url;
 }
