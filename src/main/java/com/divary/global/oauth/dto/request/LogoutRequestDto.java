32437d8bb10e11e57ef0b5b903a3bcfd686a6b30
Azin <setiguy@gachon.ac.kr>
2025-08-12 15:49:24 +0900

    feat/#89: refresh 토큰 2차 중간 커밋
    1. userID로 토큰 생성및 비교(이메일 x)
    2. 로그아웃시 deviceID가 있으면 특정 단말기 로그아웃, 없으면 전체 로그아웃. (둘다 userId와 deviceID로 비교)

---
+++ src/main/java/com/divary/global/oauth/dto/request/LogoutRequestDto.java
@@ -0,0 +1,12 @@
+package com.divary.global.oauth.dto.request;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+
+@Getter
+public class LogoutRequestDto {
+    @NotNull
+    @Schema(description = "디바이스 아이디", example = "1")
+    private String deviceId;
+}
