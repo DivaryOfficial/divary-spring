d02cb14092aac7c1911e2f85099998438f6d48e6
Azin <setiguy@gachon.ac.kr>
2025-08-12 15:49:24 +0900

    feat/#89: refresh 토큰 2차 중간 커밋
    1. userID로 토큰 생성및 비교(이메일 x)
    2. 로그아웃시 deviceID가 있으면 특정 단말기 로그아웃, 없으면 전체 로그아웃. (둘다 userId와 deviceID로 비교)

--- src/main/java/com/divary/domain/token/repository/RefreshTokenRepository.java
+++ src/main/java/com/divary/domain/token/repository/RefreshTokenRepository.java
@@ -6,6 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
     boolean existsByRefreshTokenAndDeviceId(String refreshToken, String deviceId);
     void deleteByRefreshToken(String refreshToken);
+    void deleteByUser_Id(Long user_id);
 
     RefreshToken findByUser_IdAndDeviceId(Long userId, String deviceId);
+
+    void deleteByDeviceId(String deviceId);
 }
--- src/main/java/com/divary/global/exception/ErrorCode.java
+++ src/main/java/com/divary/global/exception/ErrorCode.java
@@ -40,6 +40,7 @@ public enum ErrorCode {
     //맴버 관련
     EMAIL_NOT_FOUND(HttpStatus.NOT_FOUND, "MEMBER_001", "이메일을 찾을 수 없습니다."),
     MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "MEMBER_002", "유저를 찾을 수 없습니다."),
+    MEMBER_ALREADY_EXISTS(HttpStatus.NOT_FOUND, "MEMBER_003", "이미 가입된 이메일입니다."),
 
     //소셜 로그인 관련
     GOOGLE_BAD_GATEWAY(HttpStatus.BAD_GATEWAY, "GOOGLE_001", "구글 유저를 찾을 수 없습니다"),
