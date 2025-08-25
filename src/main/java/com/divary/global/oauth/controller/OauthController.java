fb32dacd248c262198dcd4beb3ad2b6e387dd2fc
Azin <setiguy@gachon.ac.kr>
2025-08-26 02:58:33 +0900

    feat/#89: rebase 충돌 수정

--- src/main/java/com/divary/global/oauth/controller/OauthController.java
+++ src/main/java/com/divary/global/oauth/controller/OauthController.java
@@ -6,8 +6,6 @@ import com.divary.common.response.ApiResponse;
 import com.divary.global.config.SwaggerConfig.ApiErrorExamples;
 import com.divary.global.config.SwaggerConfig.ApiSuccessResponse;
 import com.divary.global.exception.ErrorCode;
-import com.divary.global.oauth.dto.LoginRequestDto;
-import com.divary.global.oauth.dto.LoginResponseDTO;
 import com.divary.global.config.jwt.JwtResolver;
 import com.divary.global.config.security.CustomUserPrincipal;
 import com.divary.global.config.jwt.JwtTokenProvider;
@@ -34,7 +32,6 @@ public class OauthController {
 
     @PostMapping(value = "/{socialLoginType}/login")
     @Operation(summary = "로그인", description = "accessCode, device ID, 로그인 유지 여부를 보내주세요")
-    @Operation(summary = "로그인", description = "accessCode를 보내주세요")
     @ApiSuccessResponse(dataType = LoginResponseDTO.class)
     @ApiErrorExamples(value = {ErrorCode.INVALID_INPUT_VALUE})
     public ApiResponse<LoginResponseDTO> login(@PathVariable(name = "socialLoginType") SocialType socialLoginType,
